146d8f273e3c5d71b22ff940f1c6e9bb21ad648e
Mikel North <mikel.north@gmail.com>
2017-12-29 16:01:00 -0700

    initial setup and reset file

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Here are some links to websites I have Cloned</h1>
+          <p>These websites are not fully functional, but are to show my ability with css styling, flexbox, and SCSS!</p>
+        </header>
+        <body>
+          <div className='img-link'>
+          </div>
+        </body>
+
+      </div>
+    );
+  }
+}
+
+export default App;
