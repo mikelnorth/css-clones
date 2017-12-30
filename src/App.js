d65eb7741cc51fd436a927f36a3468827e91d0b4
Mikel North <mikel.north@gmail.com>
2017-12-29 18:16:35 -0700

    selected websites

--- src/App.js
+++ src/App.js
@@ -11,15 +11,38 @@ class App extends Component {
         </div>
 
         <div className='body'>
-          <div className='left'>
+          <div className='link-container1'>
             <div className='img-link1'></div>
-            <div className='img-link3'></div>
+            <div className='link-info1'>
+              <h2>Clone of nike.com</h2>
+              <p>Apple Inc. is an American multinational technology company.</p>
+            </div>
           </div>
 
-          <div className='right'>
+          <div className='link-container2'>
             <div className='img-link2'></div>
-            <div className='img-link4'></div>
+            <div className='link-info2'>
+              <h2>Clone of berkshirehathawayhs.com</h2>
+              <p>Apple Inc. is an American multinational technology company.</p>
+            </div>
           </div>
+
+          <div className='link-container3'>
+            <div className='img-link3'></div>
+            <div className='link-info3'>
+              <h2>Clone of vivint.com</h2>
+              <p>Apple Inc. is an American multinational technology company.</p>
+            </div>
+          </div>
+
+          <div className='link-container4'>
+            <div className='img-link4'></div>
+            <div className='link-info4'>
+              <h2>Clone of overstock.com</h2>
+              <p>Apple Inc. is an American multinational technology company.</p>
+            </div>
+          </div>
+
         </div>
       </div>
     );
