b3916741caebaaec02880a1b72585aab527db8ae
Mikel North <mikel.north@gmail.com>
2018-01-01 23:59:12 -0700

    nike clone outline

--- src/components/nike/Nike.js
+++ src/components/nike/Nike.js
@@ -1,14 +1,44 @@
 import React, { Component } from 'react';
 import './Nike.css';
+import logoBlack from '../../assets/air-jordan-logo-black.jpg'
 
 class Nike extends Component {
-  render() {
-    return (
-      <div className="Nike">
-        <h1>Hello Nike</h1>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="Nike">
+                <div className='header'>
+                    <div className='header-left'>
+                        <div>NikePlus</div>
+                        <div>img</div>
+                        <div>Hurley</div>
+                        <div>CONVERSE</div>
+                    </div>
+
+                    <div className='header-right'>
+                        <div>Join / Log In To Nike+ Account</div>
+                        <div>Help</div>
+                        <div>img</div>
+                        <div>img</div>
+                    </div>
+                </div>
+
+                <div className='menu'>
+                    <div className='menu-left'>
+                        <div>swoosh</div>
+                    </div>
+                    <div className='menu-center'>
+                        <div>MEN</div>
+                        <div>WOMEN</div>
+                        <div>BOYS</div>
+                        <div>GIRLS</div>
+                        <div>CUSTOMIZE</div>
+                        <div>GIFT CARDS</div>
+                    </div>
+                    <div className='menu-right'>Search bar</div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Nike;
